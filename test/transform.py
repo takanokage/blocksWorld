d5d912fa725f427a2371b1a09922f324159db66a
sujith42 <sujith2@pdx.edu>
2018-11-07 16:06:24 -0800

    Modified Reference Image

--- test/transform.py
+++ test/transform.py
@@ -13,10 +13,14 @@ imageBackground = 'white'
 
 fileType = 'PNG'
 fileDir = os.path.dirname(os.path.realpath('__file__'))
-directory = os.path.join(fileDir, '../data/transform')
+resultDirectory = os.path.join(fileDir, '../data_result/transform')
+expectedDirectory = os.path.join(fileDir, '../data_expected/transform')
 
-if not os.path.exists(directory):
-    os.makedirs(directory)
+if not os.path.exists(resultDirectory):
+    os.makedirs(resultDirectory)
+
+if not os.path.exists(expectedDirectory):
+    os.makedirs(expectedDirectory)
 
 points = np.array([
     [50,  5],
@@ -37,21 +41,44 @@ class test_transform(unittest.TestCase):
     """
     # Reference image for rotate.
     def test_rotate(self):
-        image = Image.new(imageMode, imageSize, imageBackground)
-        canvas = ImageDraw.Draw(image)
-
-        draw(canvas, points, '+')
-
-        center = np.array([180, 140])
-        draw(canvas, [center], 'center')
-
-        rotatedPoints = rotate(points, center, 180.0)
-        draw(canvas, rotatedPoints, 'X')
-
         fileName = 'test_rotate.png'
 
-        image.save(directory+"/"+fileName, fileType)
-        image.close()
+        # Result image for rotate
+        result_image = Image.new(imageMode, imageSize, imageBackground)
+        result_canvas = ImageDraw.Draw(result_image)
+        draw(result_canvas, points, '+')
+        center = np.array([180, 140])
+        draw(result_canvas, [center], 'center')
+        rotatedPoints = rotate(points, center, 90.0)
+        draw(result_canvas, rotatedPoints, 'X')
+        result_image.save(resultDirectory + "/" + fileName, fileType)
+        result_image.close()
+
+        # Expected image for rotate
+        expected_image = Image.new(imageMode, imageSize, imageBackground)
+        expected_canvas = ImageDraw.Draw(expected_image)
+        for point in points:
+            expected_canvas.text((point[0], point[1]), '+')
+        expected_canvas.text((180, 140), 'center')
+        rotatedPoints = np.array([
+            [315., 10.],
+            [305., 10.],
+            [295., 10.],
+            [285., 10.],
+            [275., 10.],
+            [265., 10.],
+            [255., 10.],
+            [245., 10.],
+        ])
+        for point in rotatedPoints:
+            expected_canvas.text((point[0], point[1]), 'X')
+        expected_image.save(expectedDirectory + "/" + fileName, fileType)
+        expected_image.close()
+
+        result = resultDirectory + "/" + fileName
+        expected = expectedDirectory + "/" + fileName
+
+        self.assertTrue(open(result, "rb").read() == open(expected, "rb").read())
 
 
 if __name__ == '__main__':
